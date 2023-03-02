f8455a6dbba48ed45e576e6f44e69758a93421b4
DevBhargav9669 <bhargavvarma96@gmail.com>
2023-03-02 19:25:24 +0530

    logo

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,7 +4,8 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="Navbar">
-      <span className="nav-logo">AlmaBetter</span>
+      {/* <span className="nav-logo">AlmaBetter</span> */}
+      <img src="https://www.almabetter.com/_next/image?url=https%3A%2F%2Falmablog-media.s3.ap-south-1.amazonaws.com%2Fimage_19400_1_5042248765.png&w=256&q=75" alt="logo" width='150px'/>
       <div className={`nav-items ${isOpen && "open"}`}>
         <a href="/home">Home</a>
         <a href="/about">My Quizzes</a>
